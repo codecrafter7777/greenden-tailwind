ada141ff4d2487be9ff86ac97f3c661b362580a9
Sania Rebeccah <saniarebeccah@Sanias-MacBook-Air.local>
2024-11-02 09:19:18 +0530

    enabled search bar

--- index.js
+++ index.js
@@ -11,3 +11,34 @@ menuicon.addEventListener("click", function() {
 closenav.addEventListener("click", function() {
     sidenavbar.style.right = "-50%"; // Use a percentage or fixed value
 });
+
+
+// Search input
+
+// var productContainer= document.getElementById("product-container")
+
+// var search = document.getElementById("search")
+
+// var productlist = productContainer.querySelectorAll("div")
+
+
+
+
+
+    var productContainer = document.getElementById("product-container");
+    var search = document.getElementById("search");
+    var productList = productContainer.querySelectorAll("div");
+
+    search.addEventListener("keyup", function (event) {
+        var enteredValue = event.target.value.toUpperCase();
+
+        for (var count = 0; count < productList.length; count++) {
+            var productName = productList[count].querySelector("h1").textContent;
+
+            if (productName.toUpperCase().indexOf(enteredValue) < 0) {
+                productList[count].style.display = "none";
+            } else {
+                productList[count].style.display = "block";
+            }
+        }
+    });
